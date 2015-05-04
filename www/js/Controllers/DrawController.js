b074963f1761e612649aa9e6175f3e3607e85cff
xszaboj <xszaboj@centrum.cz>
2015-05-04 17:46:09 +0200

    drawing
    drawing as modal window on game screen

--- www/js/Controllers/DrawController.js
+++ www/js/Controllers/DrawController.js
@@ -1,19 +1,4 @@
 ﻿angular.module('starter.controllers')
-.controller('DrawController', ['$scope', function($scope) {
-    $scope.draw = "Click to start drawing";
-    
-    $scope.width = innerWidth;
-    $scope.height = innerHeight;
-    $scope.exist = false;
-    $scope.init = function () {
-        if (!$scope.exist) {
-            $scope.exist = true;
-            var simpleboard = new DrawingBoard.Board('simple-board', {
-                controls: [{ Navigation: { back: false, forward: false } }],
-                controlsPosition: "top",
-                webStorage: false
+.controller('DrawController', ['$scope', function ($scope) {
 
-            });
-        }
-    }
 }])
--- www/js/Controllers/GameController.js
+++ www/js/Controllers/GameController.js
@@ -1,5 +1,5 @@
 ﻿angular.module('starter.controllers')
-.controller('GameController', ['$scope', '$interval', 'GameService', 'UtilService', 'MediaService', 'WordsService', function ($scope, $interval, GameService, UtilService, MediaService, WordsService) {
+.controller('GameController', ['$scope', '$interval', 'GameService', 'UtilService', 'MediaService', 'WordsService','$ionicModal', function ($scope, $interval, GameService, UtilService, MediaService, WordsService, $ionicModal) {
     //start,correct,wrong buttons
     $scope.buttonsDisabled = true;
     $scope.wordsHidden = false;
@@ -87,6 +87,7 @@
         var selected = $scope.words[0].selected ? $scope.words[0] : $scope.words[1];
         GameService.Correct(selected);
         defaultState();
+        $scope.simpleboard = null;
         UtilService.RedirectWithoutHistory("results");
     }
 
@@ -95,6 +96,7 @@
         console.log("wrong");
         GameService.Wrong();
         defaultState();
+        $scope.simpleboard = null;
         UtilService.RedirectWithoutHistory("results");
     }
     $scope.hideWords = function() {
@@ -115,6 +117,42 @@
         $scope.rangeValue = 0;
         $scope.buttonsDisabled = true;
         $scope.wordsHidden = false;
+    }
+
+
+
+    $ionicModal.fromTemplateUrl('templates/draw.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function () {
+        $scope.modal.show();
+
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.modal.remove();
+    });
+
+    $scope.width = innerWidth;
+    $scope.height = innerHeight;
+    $scope.exist = false;
+    $scope.init = function () {
+        if (!$scope.exist) {
+            $scope.exist = true;
+            $scope.simpleboard = new DrawingBoard.Board('simple-board', {
+                controls: [{ Navigation: { back: false, forward: false } }],
+                controlsPosition: "top",
+                webStorage: false
+
+            });
+        }
     }
 
 }])
